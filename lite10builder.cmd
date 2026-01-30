950e465d93169a8e8d5dc2b378c15d84250d7a9d
Thach Nguyen <thachnn80@gmail.com>
2026-01-30 20:11:41 +0700

    Prepare necessary utility binaries

--- lite10builder.cmd
+++ lite10builder.cmd
@@ -1,6 +1,9 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set "PATH=%PATH%;%~dp0bin\%PROCESSOR_ARCHITECTURE%"
+:: necessary utility binaries
+if not exist "%~dp0bin\%PROCESSOR_ARCHITECTURE%\" tar -xf bin.tar.zip "bin\%PROCESSOR_ARCHITECTURE%"
+set "PATH=%~dp0bin\%PROCESSOR_ARCHITECTURE%;%PATH%"
+
 cd /d "%~dp0"
 
