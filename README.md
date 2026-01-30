ab9a63ae7ab63c241d81b7629645b72c17d01503
Thach Nguyen <thachnn80@gmail.com>
2026-01-30 18:02:38 +0700

    Initialize files for Windows 10 ESU-Analysis hacking

---
+++ README.md
@@ -0,0 +1,3 @@
+# W10lite-builder
+
+Scripts to build a customized, trimmed-down version of Windows 10 Enterprise LTSC 21H2.
---
+++ lite10builder.cmd
@@ -0,0 +1,6 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set "PATH=%PATH%;%~dp0bin\%PROCESSOR_ARCHITECTURE%"
+cd /d "%~dp0"
+
